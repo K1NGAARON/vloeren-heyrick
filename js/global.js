c07531ffd8e75c433a541ee516cd4cb0f938e483
K1NGAARON <gerardaaron1997@gmail.com>
2023-07-08 12:05:53 +0200

    base folders

---
+++ js/global.js
@@ -0,0 +1,43 @@
+$(document).scroll(function () {
+    const header = $('.header');
+
+    if ($(this).scrollTop() > header.height()) {
+        header.addClass('active');
+    } else {
+        header.removeClass('active');
+    }
+});
+
+$("#menu-toggle").click(function() {
+    $(".small-menu-wrapper").toggle("active");
+});
+
+function closeMenu() {
+    $('.small-menu-wrapper').css('display', 'none');
+};
+
+$('.small-menu-content .nav').click(closeMenu);
+
+const cookieBanner = document.querySelector(".cookie-banner-wrapper");
+const cookiesAccepted = document.querySelector("#accept-cookies");
+const blackOverlay = document.querySelector("#overlay");
+
+// Accept cookies
+cookiesAccepted.addEventListener("click", function acceptCookies() {
+    blackOverlay.classList.add("hide-cookie-banner");
+    cookieBanner.classList.add("hide-cookie-banner");
+    storeCookieConsent();
+});
+
+function storeCookieConsent() {
+    localStorage.cookies = "accepted";
+}
+
+window.addEventListener("DOMContentLoaded", function() {
+    if ("cookies" in localStorage) {
+        blackOverlay.classList.add("hide-cookie-banner");
+        cookieBanner.classList.add("hide-cookie-banner");
+    } else {
+        // Shows cookieBanner
+    }
+});
